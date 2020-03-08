fe3a9391269c0a3bad433f89605ddb46f0e9393a
Unknown <liamwaghorn@gmail.com>
2020-03-08 13:11:46 -0230

    remove env variable for now

--- web-app/backend/app/config.py
+++ web-app/backend/app/config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/JVP-Dev"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
